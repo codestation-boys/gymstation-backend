1d9cb8a5b538d7b7e485d0e1e197ef5af12086df
ArturHamannRonconi <arturhronconi@gmail.com>
2021-06-16 12:47:34 -0300

    add authentication service and configuration

---
+++ src/config/AuthConfig.ts
@@ -0,0 +1,15 @@
+import { Secret, SignOptions } from 'jsonwebtoken'
+import Payload from '../@types/appTypes/accounts/Payload'
+
+class AuthConfig
+{
+  private constructor() {  }
+
+  public static PRIVATE_ACCESS_KEY: Secret = 'fc9a0c434c5b619c1860934b2ba90db3'
+  public static ACCESS_EXPIRES = '15m'
+  
+  public static PRIVATE_REFRESH_KEY: Secret = '14fcf8a5df83fcf7b1a08c6bd067e6a1'
+  public static REFRESH_EXPIRES = '30d'
+}
+
+export default AuthConfig
---
+++ src/modules/accounts/services/authenticationServices/CreateAuthenticationService.ts
@@ -0,0 +1,49 @@
+import { compare } from 'bcrypt'
+import { sign, SignOptions } from 'jsonwebtoken'
+
+import Authentication from '../../../../@types/appTypes/accounts/Authentication'
+import AuthConfig from '../../../../config/AuthConfig'
+import Tokens from '../../../../@types/appTypes/accounts/Tokens'
+import { BadRequestError, UnauthorizedError } from '../../../../shared/errors/errorsTypes'
+import IUserRepository from '../../interfaces/repositories/IUserRepository'
+import { inject, injectable } from 'tsyringe'
+
+@injectable()
+class CreateAuthenticationService
+{
+  constructor(
+    @inject('UserRepository')
+    private userRepository: IUserRepository
+  ) {  }
+
+  public async execute({ authorization }: Authentication): Promise<Tokens>
+  {
+    const [ hashType, hash ] = authorization.split(' ')
+
+    if(hashType !== 'Basic')
+      throw new BadRequestError('Necessary Basic Authentication!')
+
+    const [ email, password ] = Buffer.from(hash, 'base64').toString().split(':')
+
+    const userExists = await this.userRepository.findByEmail(email)
+    const passwordCorrect = await compare(password, userExists?.password ?? password)
+
+    if(!userExists || !passwordCorrect)
+      throw new UnauthorizedError('Invalid email or/and password!')
+
+    const access_payload = {}
+    const access_options: SignOptions = { 
+      expiresIn: AuthConfig.ACCESS_EXPIRES,
+      subject: userExists.id
+    }
+    const access_token = sign(
+      access_payload,
+      AuthConfig.PRIVATE_ACCESS_KEY,
+      access_options
+    )
+
+    return { access_token }
+  }
+}
+
+export default CreateAuthenticationService
