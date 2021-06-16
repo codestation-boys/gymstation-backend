76127657a37e26e53bd74a7f0170dcdd193c43f4
ArturHamannRonconi <arturhronconi@gmail.com>
2021-06-16 16:35:23 -0300

    update the route with middleware

--- src/shared/infra/http/routes/statistics.routes.ts
+++ src/shared/infra/http/routes/statistics.routes.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express'
 
+import authenticateUser from '../middlewares/AuthenticateUser'
+import getMeasuresHistoricController from '../../../../modules/statistics/controllers/measuresControllers/GetMeasuresHistoricController'
 import createMeasuresController from '../../../../modules/statistics/controllers/measuresControllers/CreateMeasuresController'
 
 const statisticsRoutes = Router()
 
-statisticsRoutes.route('/')
-  .post(createMeasuresController.handle)
+statisticsRoutes.route('/measures')
+  .get(
+    authenticateUser.handle,
+    getMeasuresHistoricController.handle
+  )
+  .post(
+    authenticateUser.handle,
+    createMeasuresController.handle
+  )
 
 export default statisticsRoutes
