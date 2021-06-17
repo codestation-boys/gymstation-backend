8dc0d2c4c5ed0362aa6491db3a43d537fd0b3fff
ArturHamannRonconi <arturhronconi@gmail.com>
2021-06-17 16:01:05 -0300

    refactoring AuthConfig

--- src/config/AuthConfig.ts
+++ src/config/AuthConfig.ts
@@ -6,9 +6,10 @@ class AuthConfig
   private constructor() {  }
 
   public static PRIVATE_ACCESS_KEY: Secret = 'fc9a0c434c5b619c1860934b2ba90db3'
-  public static ACCESS_EXPIRES = '15m'
+  public static ACCESS_EXPIRES = '20m'
   
   public static PRIVATE_REFRESH_KEY: Secret = '14fcf8a5df83fcf7b1a08c6bd067e6a1'
+  public static REFRESH_EXPIRES_NUMBER = 30
   public static REFRESH_EXPIRES = '30d'
 }
 
